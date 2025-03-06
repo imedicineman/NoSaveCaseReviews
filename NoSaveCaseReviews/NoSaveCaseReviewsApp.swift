983afde46e927974d2ec0c9af5caf96cbe73ef40
Robert Delikat <robert.delikat@gmail.com>
2025-03-06 07:07:07 -0800

    Initial Commit

---
+++ NoSaveCaseReviews/NoSaveCaseReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoSaveCaseReviewsApp.swift
+//  NoSaveCaseReviews
+//
+//  Created by Robert Delikat on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct NoSaveCaseReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
